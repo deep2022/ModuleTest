b28078f8efac68134c14b6c442f4bb55d45ec2b0
Deepak Chander Joshi <deepak.joshi@freecharge.com>
2024-01-29 12:30:36 +0530

    testing native modules

---
+++ packages/fc-package-1/README.md
@@ -0,0 +1,31 @@
+# react-native-fc-package-1
+
+none
+
+## Installation
+
+```sh
+npm install react-native-fc-package-1
+```
+
+## Usage
+
+```js
+import { multiply } from 'react-native-fc-package-1';
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ packages/fc-package-1/android/src/main/java/com/fcpackage1/FcPackage1Module.java
@@ -0,0 +1,32 @@
+package com.fcpackage1;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = FcPackage1Module.NAME)
+public class FcPackage1Module extends ReactContextBaseJavaModule {
+  public static final String NAME = "FcPackage1";
+
+  public FcPackage1Module(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
